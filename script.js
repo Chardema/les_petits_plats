83c9de437ae9b23a11f836ca132c45b03c1d34e7
HARDEMAN CHRISTOPHER <christo59@icloud.com>
2022-08-12 15:44:01 +0200

    :construction: trying some things

--- script.js
+++ script.js
@@ -94,3 +94,90 @@ let quickSort = (array, left, right) => {
 	}
 	return array;
 }
+
+let partition = (array, left, right) => {
+	let pivot = array[Math.floor((right + left) / 2)]; //middle element
+
+	while (left <= right) {
+		while (array[left].localeCompare(pivot) < 0) {
+			left++;
+		}
+		while (array[right].localeCompare(pivot) > 0) {
+			right--;
+		}
+		if (left <= right) {
+			swap(array, left, right);
+			left++;
+			right--;
+		}
+	}
+	return left;
+}
+//function to swap position
+let swap = (items, leftIndex, rightIndex) => {
+	var temp = items[leftIndex];
+	items[leftIndex] = items[rightIndex];
+	items[rightIndex] = temp;
+}
+//filter and extract only id, name, ingredients, and description
+let createFilteredArr = (arr) => {
+	let filteredArr = [];
+	for (let i = 0; i<arr.length; i++) {
+		let filtered = (({id, ingredients, name, description}) => ({id, ingredients, name, description}))(arr[i][1]);
+		filteredArr.push(filtered);
+	}
+	return filteredArr;
+}
+let filteredArr = createFilteredArr(recipesArray);
+
+function FilterKeyword(item) {
+	this.id = item.id;
+	let thisIngre = item.ingredients.map(b => b.ingredient.toLowerCase()).flat();
+	let keywordString = item.name + " " + thisIngre + " " + item.description;
+	let uniqueValue = [...new Set(keywordString.split(/[\s,().]+/))];
+	this.keyword = quickSort(uniqueValue, 0, uniqueValue.length-1);
+}
+
+let extractKeyword = (arr) => {
+	let newArr = [];
+	for (let i=0; i<arr.length; i++) {
+		let keyword = new FilterKeyword(arr[i]);
+		newArr.push(keyword);
+	}
+	return newArr;
+}
+let filteredKeywordArr = extractKeyword(filteredArr);
+console.log(filteredKeywordArr);
+//take only the keywords
+let allKeywords = [];
+filteredKeywordArr.forEach(item => {allKeywords.push(item.keyword)});
+let flatKeyword = allKeywords.flat();
+let allKeywordsLowerCase = [];
+flatKeyword.forEach(word => {allKeywordsLowerCase.push(word.toLowerCase())});
+let searchOptionsNotSorted = [...new Set(allKeywordsLowerCase.flat())];
+//sort by alphabetical order
+let searchOptions = quickSort(searchOptionsNotSorted, 0, searchOptionsNotSorted.length-1);
+console.log(searchOptions);
+
+function KeywordObject(item) {
+	this.keyword = item;
+	let recipeIds = [];
+	for (let i=0; i<filteredKeywordArr.length; i++) {
+		if (filteredKeywordArr[i].keyword.indexOf(item) >= 0 ) {
+			recipeIds.push(filteredKeywordArr[i].id);
+		}
+	}
+	this.ids = recipeIds;
+}
+
+let keywordArray = (arr) => {
+	let newArr = [];
+	for (let i=0; i<arr.length; i++) {
+		let keyword = new KeywordObject(arr[i]);
+		newArr.push(keyword);
+	}
+	return newArr;
+}
+
+let keywordObjectArray = keywordArray(searchOptions);
+console.log(keywordObjectArray);
